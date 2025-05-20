fd56206fcb91807d96932ee31ff2a795a057a9cd
avery <grimm4253@gmail.com>
2025-05-20 11:54:01 -0700

    G

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import "./app.css";
+import "./App.css";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Main from "./Main/Main";
