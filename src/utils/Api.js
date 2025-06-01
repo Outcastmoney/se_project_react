220a95a3544956ff8225599148d8494d506d1fb9
avery <grimm4253@gmail.com>
2025-06-01 10:19:49 -0700

    NEW

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,25 +1,21 @@
 const BASE_URL = "http://localhost:3001";
 
+export function checkResponse(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    return Promise.reject(`Error: ${res.status}`);
+  }
+}
+
 function getItems() {
-  return fetch(`${BASE_URL}/items`).then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject(`Error: ${res.status}`);
-    }
-  });
+  return fetch(`${BASE_URL}/items`).then(checkResponse);
 }
 
 function deleteItem(id) {
   return fetch(`${BASE_URL}/items/${id}`, {
     method: "DELETE",
-  }).then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject(`Error: ${res.status}`);
-    }
-  });
+  }).then(checkResponse);
 }
 
 function addItem({ name, imageUrl, weather }) {
@@ -27,13 +23,7 @@ function addItem({ name, imageUrl, weather }) {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, imageUrl, weather })
-  }).then(res => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject(`Error: ${res.status}`);
-    }
-  });
+  }).then(checkResponse);
 }
 
 export default { getItems, deleteItem, addItem };
